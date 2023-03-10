66d4d2df374ce67d7d23a717a93124a1f301fa80
jackidrogeo99 <127427429+jackidrogeo99@users.noreply.github.com>
2023-03-10 17:57:13 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -1,6 +1,17 @@
 # My first code in GitHub
 # Let's install the raster package
 
-install.packages("raster")
+install.packages("raster") #Si usano le "" perchè si esce da R
 
 library(raster) 
+# Import data, setting the working directory
+setwd("C:/Telerilevamento Geoecologico/Lab/") #Windows
+
+l2011<-brick("p224r63_2011_masked.grd")
+# Plotting the data in a savage manner
+plot(l2011)
+cl<-colorRampPalette(c("red", "orange", "yellow"))(100) # I colori tra "" perchè così sono immagazzinati su R. 100 sono le sfumature
+plot(l2011, col=cl)
+# Plotting one el
+nir<-l2011[[4]] # or: nir<-l2011$B4_sre
+plot(nir, col=cl) 
