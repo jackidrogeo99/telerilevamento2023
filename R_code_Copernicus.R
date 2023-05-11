212ed4dc83074f0e1298849b7bdd95d69b6585cc
jackidrogeo99 <127427429+jackidrogeo99@users.noreply.github.com>
2023-05-11 17:52:48 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -4,7 +4,7 @@ install.packages("ncdf4")
 library(ncdf4)
 setwd("C:/Telerilevamento_Geoecologico/Lab/")
 # dataset: c_gls_SSM1km_202305030000_CEURO_S1CSAR_V1.2.1.nc
-
+sc<-raster("c_gls_SSM1km_202305030000_CEURO_S1CSAR_V1.2.1.nc")
 
 
 
