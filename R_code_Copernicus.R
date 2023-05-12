500f2863698ec2818a3d2c6b3f4344ea29aff648
jackidrogeo99 <127427429+jackidrogeo99@users.noreply.github.com>
2023-05-12 16:44:33 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -1,6 +1,6 @@
 # R code for downloading and visualising Copernicus data
 library(raster)
-install.packages("ncdf4")
+install.packages("ncdf4") # Serve per leggere i dati .nc
 library(ncdf4)
 setwd("C:/Telerilevamento_Geoecologico/Lab/")
 # dataset: c_gls_SSM1km_202305030000_CEURO_S1CSAR_V1.2.1.nc
